fb7a3a8e6011e374e09ebca39eb2737aaebfd45f
Jondrewd <jonas220703@gmail.com>
2024-03-01 09:14:16 -0300

    Criando as posições

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,7 +9,6 @@ import chesspieces.Rook;
 public class ChessMatch {
 
     private Board board;
-    
    /*  public int turn;
     public Color currentPlayer;
     public Boolean check;
@@ -29,11 +28,12 @@ public class ChessMatch {
         } 
         return mat;
     }
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0, 7));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 1));
-        
-        
+        placeNewPiece('a', 3, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 1,new Rook(board, Color.WHITE));
+        placeNewPiece('a', 2 ,new King(board, Color.BLACK));      
     }
 }
